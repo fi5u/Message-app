dd8b5d675060f1007c335e36a51ae1deee0b09f6
Tommy Fisher <tommybfisher@gmail.com>
2015-08-25 11:41:51 +0300

    Form validation

--- message_app/forms.py
+++ message_app/forms.py
@@ -3,5 +3,5 @@ from wtforms import StringField, PasswordField
 from wtforms.validators import DataRequired
 
 class LoginForm(Form):
-    username = StringField('username')
-    password = PasswordField('password')
+    username = StringField('username', validators=[DataRequired()])
+    password = PasswordField('password', validators=[DataRequired()])
