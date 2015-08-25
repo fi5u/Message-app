63e3fa9614226d97d20f22de714c0c8ab2d789d4
Tommy Fisher <tommybfisher@gmail.com>
2015-08-25 09:32:53 +0300

    Login functional

--- message_app/views.py
+++ message_app/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, session
 from message_app import message_app
 from .forms import LoginForm
 
@@ -10,17 +10,29 @@ def index():
 @message_app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if session['authenticated'] == True:
+        return redirect('/admin')
     if form.validate_on_submit():
         if form.username.data == 'admin' and form.password.data == '123':
+            session['authenticated'] = True
             return redirect('/admin')
     return render_template('login.html', title='login', form=form)
 
 @message_app.route('/admin')
 def admin():
+    if session['authenticated'] == False:
+        return redirect('/login')
     return render_template('admin.html',
         title='admin')
+
+@message_app.route('/logout')
+def logout():
+    session['authenticated'] = False
+    return redirect('/login')
 
 @message_app.route('/about')
 def about():
     return render_template('about.html',
         title='about')
+
+message_app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
